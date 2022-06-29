22af306773b8ea87187e0f7900eda49e37ab2138
Koaha <ldvankhoa@gmail.com>
2022-06-29 11:55:18 +0700

    add test

--- tests/test_AR_RR.py
+++ tests/test_AR_RR.py
@@ -1,8 +1,10 @@
-import pytest
+# import pytest
+import numpy as np
 from RRest.estimate_rr.AR_RR import get_rr
 
 def test_get_rr():
-    mock_sig = [1]*100
-    res = get_rr(mock_sig,10)
+    mock_sig = np.random.random(10000)
+    # res = get_rr(mock_sig,10)
+    res = -1
     assert res > 0
     pass
