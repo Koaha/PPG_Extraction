05b26712d1cfbf5904f3c495e996eebaa727a87c
Koaha <ldvankhoa@gmail.com>
2022-06-29 11:45:14 +0700

    restructure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='1.0',
     packages=find_packages(include=['RRest.*']),
     url='https://github.com/Koaha/RRest',
-    install_reqs = parse_requirements('requirements.txt', session='hack'),
+    install_reqs = parse_requirements('requirements.txt', session='deploy'),
     python_requires='>=3.6',
     license='MIT',
     author='Koha',
